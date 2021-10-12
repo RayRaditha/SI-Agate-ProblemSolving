7dc2a414a38b6673497a2d8d78e4cfb117a2eac7
RayRaditha <puturadithaa@gmail.com>
2021-10-12 23:10:01 +0700

    Problem Enam

---
+++ Assets/Scripts/BoxObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxObject : MonoBehaviour
+{
+    public GameObject Kotak;
+    public float xMin, xMax;
+    public float yMin, yMax;
+    public float randomX, randomY;
+
+    // Function untuk melakukan Spawn Kotak secara Random
+    void SpawnBox()
+    {
+        int count = Random.Range(5, 10);
+
+        for (int i = 0; i < count; i++)
+        {
+            randomX = Random.Range(xMin, xMax);
+            randomY = Random.Range(yMin, yMax);
+            Instantiate(Kotak, new Vector2(randomX, randomY), Quaternion.identity);
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnBox();
+    }
+
+}
